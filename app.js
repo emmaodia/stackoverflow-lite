09f237befed59081efc5eb260c5a6be40a503adb
emmaodia <odia.emma@gmail.com>
2018-08-28 17:15:21 +0100

    [ch-#160098928-Set Up App.js]

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+module.exports = app;
