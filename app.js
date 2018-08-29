b0c293d5095bfb7f08ab7df26b827d23a57390bd
emmaodia <odia.emma@gmail.com>
2018-08-29 18:57:43 +0100

    [ch-#160098928-Set Up dotenv]

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
