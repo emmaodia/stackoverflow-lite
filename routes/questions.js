4d824cada596e3455943cf76bb9f42ac81fef20f
emmaodia <odia.emma@gmail.com>
2018-09-02 15:49:35 +0100

    [ft-POST questions completed]

--- routes/questions.js
+++ routes/questions.js
@@ -4,9 +4,6 @@ var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/stackoverflow-lite';
 
 router.get('/', (req, res, next) => {
-  // res.status(200).json({
-  //   message : "GET requests to the questions end point"
-  // });
   pg.connect( connectionString, (err, client, done) => {
     if(err){
       return console.error('error fetching client from pool', err);
@@ -26,11 +23,6 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  // res.status(201).json({
-  //   message : 'This is a success POST request to questions'
-  // });
-  //var results = [];
-
   //Grab data from Http requests
   var data = {
     title: req.body.title,
@@ -47,17 +39,8 @@ router.post('/', (req, res, next) => {
     client.query('INSERT INTO public.questions(title, body) values($1, $2)',
       [data.title, data.body]);
         });
-    res.status(200).json({message: "It works"});
-    // var query = client.query('SELECT * FROM questions ORDER BY id ASC');
-    //
-    // query.on('row', row => {
-    //   results.push(row);
-    // });
-    //
-    // query.on('end', () => {
-    //   done();
-    //   return res.status(201).json("It works");
-    // });
+
+    res.status(201).json({message: "It works"});
 });
 
 module.exports = router;
