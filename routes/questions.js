f408e6c79dce2df7c18e64881dd6a7219a35ac26
emmaodia <odia.emma@gmail.com>
2018-09-02 12:43:10 +0100

    [ft-fetch recipes using GET from DB completed]

--- routes/questions.js
+++ routes/questions.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/stackoverflow-lite';
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message : "GET requests to the questions end point"
+  // res.status(200).json({
+  //   message : "GET requests to the questions end point"
+  // });
+  pg.connect( connectionString, (err, client, done) => {
+    if(err){
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT * FROM public.questions', (err, result) => {
+
+      if(err){
+        return console.error('error running query', err);
+      }
+      // console.log(result.rows)
+      res.status(200).json({
+        Questions: result.rows
+      });
+      done();
+    });
   });
 });
 
