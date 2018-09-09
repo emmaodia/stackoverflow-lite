1bc53f7c613ef2aab4001a82ee589c52d6cceefb
emmaodia <odia.emma@gmail.com>
2018-09-09 16:16:09 +0100

    [ft-#160261199-POST answers endpoint working]

--- routes/answers.js
+++ routes/answers.js
@@ -28,4 +28,36 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.post('/', (req, res, next) => {
+  //Grab data from Http requests
+  var data = {
+    content: req.body.content
+  }
+
+  pg.connect(connectionString, (err, client, done) => {
+    if(err){
+      done();
+      console.error('error fetching client from pool', err);
+      res.status(500).json({ success: false, data: err});
+    }
+
+    client.query('INSERT INTO public.answers(content) values($1)',
+      [data.content]);
+      done();
+
+    client.query('SELECT * FROM public.answers', (err, result) => {
+
+      if(err){
+        return console.error('error running query', err);
+      }
+      // console.log(result.rows)
+      res.status(201).json({
+        Answers: result.rows
+      });
+      done();
+    });
+  });
+    //res.status(201).json({message: "It works"});
+});
+
 module.exports = router;
